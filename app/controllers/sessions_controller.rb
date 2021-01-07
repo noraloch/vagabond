6fd65b661e6fea1a9dcd1bd99df213b2283aaff6
jkang91 <jkang34@gmail.com>
2021-01-06 23:59:00 -0500

    added search bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            redirect_to places_path(@places)
         else
             flash[:errors] = "Username or Password incorrect"
             redirect_to new_session_path
